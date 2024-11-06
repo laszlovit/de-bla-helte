de62abfbf645877bb24fe2d7b27bc9b6bce108ec
laszlovit <laszlo.vitkai@gmail.com>
2024-11-06 09:00:01 +0100

    added temporary actual client logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,14 +100,14 @@ function LogoClouds() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <Image
             alt="Transistor"
-            src="/home/place-to-logo.png"
+            src="/home/rekom-group-logo.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
           <Image
             alt="Reform"
-            src="/home/rustik-logo.webp"
+            src="/home/henne-molle-hotel-logo.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
@@ -126,14 +126,14 @@ function LogoClouds() {
           </svg>
           <Image
             alt="SavvyCal"
-            src="https://tailwindui.com/plus/img/logos/158x48/savvycal-logo-gray-900.svg"
+            src="/home/dansk-bilglas-logo.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
           />
           <Image
             alt="Statamic"
-            src="https://tailwindui.com/plus/img/logos/158x48/statamic-logo-gray-900.svg"
+            src="/home/stennevad-logo.png"
             width={158}
             height={48}
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
