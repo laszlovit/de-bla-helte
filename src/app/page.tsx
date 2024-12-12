9bb1283c818d3e3fee74023663e042660370dc2b
laszlovit <laszlo.vitkai@gmail.com>
2024-12-12 13:35:20 +0100

    escaped ' character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ function LogoCloud() {
 		<section id="relume" className="overflow-hidden py-12 md:py-16 lg:py-20">
 			<div className="container mb-8 w-full max-w-lg px-[5%] md:mb-10 lg:mb-12">
 				<h1 className="text-center text-base font-bold leading-[1.2] md:text-md md:leading-[1.2]">
-					Used by the world's leading companies
+					Used by the world&apos;s leading companies
 				</h1>
 			</div>
 			<div className="flex items-center pt-7 md:pt-0">
