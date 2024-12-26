253c417733bbf6bf72aee2b593c80eda07a618a9
laszlovit <laszlo.vitkai@gmail.com>
2024-12-26 13:46:23 +0100

    added optimize package in next config for the relume library

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,17 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
-    ],
-  },
-}
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "cdn.sanity.io",
+			},
+		],
+	},
+	experimental: {
+		optimizePackageImports: ["@relume_io/relume-ui", "@relume_io/relume-tailwind"],
+	},
+};
 
-export default nextConfig
+export default nextConfig;
